0d87efbf8ad77264981f20ad54433391648cb67a
Yuni0221 <moonrise002002@gmail.com>
2023-02-11 15:56:32 +0900

    new update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,37 @@
 import styles from "./Product.module.css";
 
 import sweater from "../assets/sweater.jpg";
+import bag from "../assets/bag.jpg";
 
 function Product() {
   return (
     <section className={styles.section}>
       <h2 className={styles.subject}>패션</h2>
-      <div className={styles.grid}>
-        <a className={styles.card} href="/">
+      <div className={styles.productList}>
+        <a className={styles.productCard} href="/">
           <figure>
             <img className={styles.image} src={sweater} alt="sweater" />
           </figure>
+          <div className={styles.productInfo}>
+            <p className={styles.productTitle}>Lemaire - Very Soft Wool Knit</p>
+            <p className={styles.price}>$122</p>
+          </div>
         </a>
-        <a className={styles.card} href="/">
+        <a className={styles.productCard} href="/">
+          <figure>
+            <img className={styles.image} src={bag} alt="bag" />
+          </figure>
+          <div className={styles.productInfo}>
+            <p className={styles.productTitle}>
+              Quiting Chain Luxury baguette Bag
+            </p>
+            <p className={styles.price}>$280</p>
+          </div>
+        </a>
+        <a className={styles.productCard} href="/">
           a
         </a>
-        <a className={styles.card} href="/">
-          a
-        </a>
-        <a className={styles.card} href="/">
+        <a className={styles.productCard} href="/">
           a
         </a>
       </div>
