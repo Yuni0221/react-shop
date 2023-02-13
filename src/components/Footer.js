d47063ceeaa95db4ee6b35a4f029fab62ec570ea
Yuni0221 <moonrise002002@gmail.com>
2023-02-13 12:08:17 +0900

    new update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,53 @@
 import styles from "./Footer.module.css";
 
+import { ReactComponent as Visa } from "../assets/Visa.svg";
+import { ReactComponent as Facebook } from "../assets/facebook.svg";
+import { ReactComponent as Instagram } from "../assets/instagram.svg";
+import { ReactComponent as Github } from "../assets/github.svg";
+
 function Footer() {
   return (
-    <div className={styles.footer}>
-      Footer<div></div>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.linkFooter}>
+        <a className={styles.zerobase} href="https://zero-base.co.kr">
+          제로베이스
+        </a>
+      </div>
+      <ul className={styles.cardList}>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+        <li>
+          <Visa className={styles.visa} />
+        </li>
+      </ul>
+      <div className={styles.link}>
+        <a href="https://facebook.com/0base">
+          <Facebook className={styles.facebook} />
+        </a>
+        <a href="https://instagram.com/zerobase.official">
+          <Instagram className={styles.instagram} />
+        </a>
+        <a href="https://github.com/Yuni0221">
+          <Github className={styles.github} />
+        </a>
+      </div>
+      <div>
+        <p className={styles.copyright}>Copyright © 2023 Zero Base</p>
+      </div>
+    </footer>
   );
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,6 +6,12 @@ import hat from "../assets/hat.jpg";
 import onepiece from "../assets/onepiece.jpg";
 import ring from "../assets/ring.jpg";
 import redring from "../assets/redring.jpg";
+import bluenecklace from "../assets/bluenecklace.jpg";
+import pearl from "../assets/pearl.jpg";
+import camera from "../assets/camera.jpg";
+import phone from "../assets/phone.jpg";
+import audio from "../assets/audio.jpg";
+import earphone from "../assets/earphone.jpg";
 
 function Product() {
   return (
@@ -81,22 +87,24 @@ function Product() {
           </a>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={hat} alt="hat" />
+              <img
+                className={styles.image}
+                src={bluenecklace}
+                alt="bluenecklace"
+              />
             </figure>
             <div className={styles.productInfo}>
-              <p className={styles.productTitle}>
-                Quiting Chain Luxury baguette Bag
-              </p>
-              <p className={styles.price}>$88</p>
+              <p className={styles.productTitle}>Blue Aquamarine Necklace</p>
+              <p className={styles.price}>$312</p>
             </div>
           </a>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={onepiece} alt="onepiece" />
+              <img className={styles.image} src={pearl} alt="pearl" />
             </figure>
             <div className={styles.productInfo}>
-              <p className={styles.productTitle}>Navy Punching onepiece</p>
-              <p className={styles.price}>$140</p>
+              <p className={styles.productTitle}>4 Colors Pearl Earring Set</p>
+              <p className={styles.price}>$200</p>
             </div>
           </a>
         </div>
@@ -107,44 +115,42 @@ function Product() {
         <div className={styles.productList}>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={sweater} alt="sweater" />
+              <img className={styles.image} src={camera} alt="camera" />
             </figure>
             <div className={styles.productInfo}>
-              <p className={styles.productTitle}>
-                Lemaire - Very Soft Wool Knit
-              </p>
-              <p className={styles.price}>$122</p>
+              <p className={styles.productTitle}>80's Retro Analogue Camera</p>
+              <p className={styles.price}>$2000</p>
             </div>
           </a>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={bag} alt="bag" />
+              <img className={styles.image} src={phone} alt="phone" />
             </figure>
             <div className={styles.productInfo}>
               <p className={styles.productTitle}>
-                Quiting Chain Luxury baguette Bag
+                256GB PSD SmartPhone - Space Grey color
               </p>
-              <p className={styles.price}>$280</p>
+              <p className={styles.price}>$980</p>
             </div>
           </a>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={hat} alt="hat" />
+              <img className={styles.image} src={audio} alt="audio" />
             </figure>
             <div className={styles.productInfo}>
-              <p className={styles.productTitle}>
-                Quiting Chain Luxury baguette Bag
-              </p>
-              <p className={styles.price}>$88</p>
+              <p className={styles.productTitle}>Musical Button Streo Audio</p>
+              <p className={styles.price}>$190</p>
             </div>
           </a>
           <a className={styles.productCard} href="/">
             <figure>
-              <img className={styles.image} src={onepiece} alt="onepiece" />
+              <img className={styles.image} src={earphone} alt="earphone" />
             </figure>
             <div className={styles.productInfo}>
-              <p className={styles.productTitle}>Navy Punching onepiece</p>
-              <p className={styles.price}>$140</p>
+              <p className={styles.productTitle}>
+                Earbuds3 Silver Color with Space Grey Case
+              </p>
+              <p className={styles.price}>$199</p>
             </div>
           </a>
         </div>
